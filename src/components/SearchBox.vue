a486d2f428a3a9a8477255967c1ccb8d9b8604f8
Michal <michal.biernat@currenda.pl>
2022-09-22 16:01:34 +0200

    style search box

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,6 @@
+<template>
+  <form class="search__form">
+    <button type="submit" class="search__btn"></button>
+    <input type="search" placeholder="Search..." class="search__input" />
+  </form>
+</template>
