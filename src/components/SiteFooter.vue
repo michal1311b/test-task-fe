67bfdf30473a3e5cb11cefa06e873e4c509a1663
Michal <michal.biernat@currenda.pl>
2022-09-22 15:49:44 +0200

    build site footer

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import IconLogo from "@/components/icons/IconLogo.vue";
+
+export default defineComponent({
+  components: { IconLogo },
+});
+</script>
+
+<template>
+  <footer class="footer__container">
+    <a href="https://github.com" title="Github main page"><IconLogo /></a>
+    <span class="footer__text">© All rights reserved, 2022</span>
+  </footer>
+</template>
+
+<style scoped></style>
