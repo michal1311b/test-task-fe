fe9a50cb7eb685d72e4f10aefb2d99034bf51586
Michal <michal.biernat@currenda.pl>
2022-09-22 16:07:25 +0200

    build ListItem component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import IconRubenDuck from "@/components/icons/IconRubenDuck.vue";
+import IconAnchor from "@/components/icons/IconAnchor.vue";
+
+export default defineComponent({
+  props: { user: { type: Object, default: null } },
+  components: { IconRubenDuck, IconAnchor },
+});
+</script>
+
+<template>
+  <IconRubenDuck :avatarSrc="user.avatar_url" />
+
+  <div class="text-center">
+    <h1 class="title__userAvatar title__userAvatar--space">
+      {{ user.login }}
+    </h1>
+    <a :href="user.html_url" target="_blank"><IconAnchor /> github</a>
+  </div>
+</template>
+
+<style></style>
