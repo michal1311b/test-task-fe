c99ee0051223c04b30df1e4593e393ac78f333f8
Michal <michal.biernat@currenda.pl>
2022-09-22 21:32:38 +0200

    set up vuex in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,36 @@ import { createApp } from "vue";
 import { createStore } from "vuex";
 import App from "./App.vue";
 import router from "./router";
+import { fetchUsers } from "./services/github.service";
 
 import "./assets/main.css";
 
 const store = createStore({
-  /* state, actions, mutations */
+  state() {
+    return {
+      userList: [],
+      currentUser: null,
+    };
+  },
+  actions: {
+    async FETCH_USERS(context) {
+      const response: any = await fetchUsers();
+
+      if (response) {
+        context.commit("SET_USER_LIST", response);
+      }
+    },
+  },
+  mutations: {
+    SET_USER_LIST(state, list) {
+      state.userList = list;
+    },
+  },
+  getters: {
+    GET_USER_LIST(state) {
+      return state.userList;
+    },
+  },
 });
 
 const app = createApp(App);
